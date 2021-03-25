75889fdc8122048d25bb6817d10ad3416ba1d885
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-03-24 19:41:16 -0700

    removed dot from constant docstrings

--- game.py
+++ game.py
@@ -11,55 +11,55 @@ from time import sleep
 
 
 def RED():
-    """Return ANSI escape for text color red.
+    """Return ANSI escape for text color red
     """
     return "\033[31m"
 
 
 def BLUE():
-    """Return ANSI escape for text color blue.
+    """Return ANSI escape for text color blue
     """
     return "\033[34m"
 
 
 def GREEN():
-    """Return ANSI escape for text color green.
+    """Return ANSI escape for text color green
     """
     return "\033[32m"
 
 
 def YELLOW():
-    """Return ANSI escape for text color yellow.
+    """Return ANSI escape for text color yellow
     """
     return "\033[33m"
 
 
 def CYAN():
-    """Return ANSI escape for text color cyan.
+    """Return ANSI escape for text color cyan
     """
     return "\033[36m"
 
 
 def END():
-    """Return ANSI escape to terminate previous ANSI escape codes.
+    """Return ANSI escape to terminate previous ANSI escape codes
     """
     return "\033[0m"
 
 
 def BOLD():
-    """Return ANSI escape for bold text.
+    """Return ANSI escape for bold text
     """
     return "\033[01m"
 
 
 def UNDERLINE():
-    """Return ANSI escape for text underline.
+    """Return ANSI escape for text underline
     """
     return "\033[04m"
 
 
 def BLINK():
-    """Return ANSI escape for text blink.
+    """Return ANSI escape for text blink
     """
     return "\033[05m"
 
