d1f2a711717ba27bb63d8774464a52803826b01f
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-03-28 14:30:57 -0700

    Add unittest for roll_for_first_hit

--- test_roll_for_first_hit.py
+++ test_roll_for_first_hit.py
@@ -18,8 +18,17 @@ class TestRollForFirstHit(TestCase):
                    "Tom has the first move!\n\n"
         self.assertEqual(game_print, expected)
 
+    @patch('random.randint', side_effect=[20, 50])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_roll_for_first_hit_foe_first(self, mock_output, mock_random_number):
+        test_player = {'name': 'Kerry'}
+        test_feo = {'name': 'Tom'}
+        roll_for_first_hit(test_player, test_feo)
+        game_print = mock_output.getvalue()
+        expected = "\nKerry has rolled a 20\nTom has rolled a 50\n\n" \
+                   "Tom has the first move!\n\n"
+        self.assertEqual(game_print, expected)
 
 
 if __name__ == "__main__":
     main()
-
