e4a758a0c006b96386124c8220e812f22c0f29c4
Kerry Chow <kerry_chow@hotmail.ca>
2021-03-28 14:10:19 -0700

    Add unit test test_attack_miss

--- test_attack.py
+++ test_attack.py
@@ -25,3 +25,24 @@ class TestAttack(TestCase):
         actual = test_receiver["HP"]
         expected = 10
         self.assertEqual(expected, actual)
+
+    @patch('game.hit_or_miss', return_value=False)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_attack_miss(self, mock_output, mock_boolean):
+        test_attacker = {"min_damage": 1,
+                         "max_damage": 15,
+                         "hit_rate": 1,
+                         "HP": 15,
+                         "name": "attacker",
+                         "attack_move": "punch"}
+        test_receiver = {"min_damage": 1,
+                         "max_damage": 15,
+                         "hit_rate": 80,
+                         "HP": 15,
+                         "name": "receiver",
+                         "attack_move": "kick"}
+        attack(test_attacker, test_receiver)
+        the_game_printed_this = mock_output.getvalue()
+        expected_output = "attacker used \033[31mpunch\033[0m!\n" \
+                          "attacker missed! | receiver HP:15\n\n"
+        self.assertEqual(expected_output, the_game_printed_this)
