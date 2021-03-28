92ce1db8283946723650c9388e27926b07e65417
Kerry Chow <kerry_chow@hotmail.ca>
2021-03-27 22:50:43 -0700

    Update docstrings for ANSI escape constants

--- game.py
+++ game.py
@@ -10,61 +10,73 @@ from time import sleep
 
 
 def RED():
-    """Return ANSI escape for text color red
+    """Return ANSI escape code for text color red
+
+    :return: ANSI escape code for text color red
     """
     return "\033[31m"
 
 
 def BLUE():
-    """Return ANSI escape for text color blue
+    """Return ANSI escape code for text color blue
+
+    :return: ANSI escape code for text color blue
     """
     return "\033[34m"
 
 
 def GREEN():
-    """Return ANSI escape for text color green
+    """Return ANSI escape code for text color green
+
+    :return: ANSI escape code for text color green
     """
     return "\033[32m"
 
 
 def YELLOW():
-    """Return ANSI escape for text color yellow
+    """Return ANSI escape code for text color yellow
+
+    :return: ANSI escape code for text color yellow
     """
     return "\033[33m"
 
 
-def CYAN():
-    """Return ANSI escape for text color cyan
-    """
-    return "\033[36m"
-
-
 def PURPLE():
-    """Return ANSI escape for text color purple.
+    """Return ANSI escape code for text color purple.
+
+    :return: ANSI escape code for text color purple
     """
     return "\033[35m"
 
 
 def END():
-    """Return ANSI escape to terminate previous ANSI escape codes
+    """Return ANSI escape code to terminate previous ANSI escape codes.
+
+    :return: ANSI escape code to terminate previous ANSI escape codes
     """
     return "\033[0m"
 
 
 def BOLD():
-    """Return ANSI escape for bold text
+    """Return ANSI escape code for bold text.
+
+    :return: ANSI escape code for bold text
     """
     return "\033[01m"
 
 
 def UNDERLINE():
-    """Return ANSI escape for text underline
+    """ANSI escape code for text underline.
+
+    :return: ANSI escape code for underlined text
     """
     return "\033[04m"
 
 
 def BLINK():
-    """Return ANSI escape for text blink
+    """Return ANSI escape code for text blink.
+
+    :return: ANSI escape code for blinking text
     """
     return "\033[05m"
 
