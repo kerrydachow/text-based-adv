963e87890a8d470ece1eb2366bb40f7c854aa9c7
Kerry Chow <kerry_chow@hotmail.ca>
2021-03-28 10:38:42 -0700

    Fix unit test for test_class_choice

--- test_class_choice.py
+++ test_class_choice.py
@@ -9,13 +9,13 @@ class TestClassChoice(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=["0"])
     def test_class_choice_return_string(self, mock_input, mock_output):
-        class_choice()
+        actual = class_choice()
         expected = str
-        self.assertEquals(type(mock_output), expected)
+        self.assertEqual(type(actual), expected)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     @patch('builtins.input', side_effect=["0"])
     def test_class_choice_return_correct_class(self, mock_input, mock_output):
-        class_choice()
+        actual = class_choice()
         expected = "Sorcerer"
-        self.assertEqual(mock_output, expected)
+        self.assertEqual(actual, expected)
