a0a4fd2367019d756a529aacfee5bfe10b3fff77
Kerry Chow <kerry_chow@hotmail.ca>
2021-03-28 03:52:57 -0700

    Add upper_bound unit test

--- test_print_map.py
+++ test_print_map.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from game import print_map
+from game import BOSS_LOCATION
 from unittest import mock
 import io
 
@@ -17,4 +18,18 @@ class TestPrintMap(TestCase):
                    "[ ][ ][ ][ ][ ]\n" \
                    "[ ][ ][ ][ ][ ]\n" \
                    "\033[05m😇\033[0m [ ][ ][ ][ ]\n"
+        self.assertEqual(mock_stdout.getvalue(), expected)
+
+    @mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_map_player_upper_bound(self, mock_stdout):
+        game_board = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3),
+                      (1, 4), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (3, 0), (3, 1), (3, 2),
+                      (3, 3), (3, 4), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4)]
+        player_dictionary = {"location": [4, 4]}
+        print_map(5, game_board, player_dictionary)
+        expected = "[ ][ ][ ][ ]\033[05m😇\033[0m \n"\
+                   "[ ][ ][ ][ ][ ]\n" \
+                   "[ ][ ][ ][ ][ ]\n" \
+                   "[ ][ ][ ][ ][ ]\n" \
+                   "[ ][ ][ ][ ][ ]\n"
         self.assertEqual(mock_stdout.getvalue(), expected)
