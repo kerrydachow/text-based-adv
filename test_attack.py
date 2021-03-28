443cc13145fd7b957d96099902fa286986edaf5f
Kerry Chow <kerry_chow@hotmail.ca>
2021-03-28 14:17:56 -0700

    Add unit test test_attack_attacker_dead and modify unit test names

--- test_attack.py
+++ test_attack.py
@@ -8,7 +8,7 @@ from game import attack
 class TestAttack(TestCase):
     @patch('random.randint', return_value=5)
     @patch('game.hit_or_miss', return_value=True)
-    def test_attack_return_hp(self, mock_randint, mock_boolean):
+    def test_attack_receiver_return_hp(self, mock_randint, mock_boolean):
         test_attacker = {"min_damage": 5,
                          "max_damage": 15,
                          "hit_rate": 1,
@@ -28,7 +28,7 @@ class TestAttack(TestCase):
 
     @patch('game.hit_or_miss', return_value=False)
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_attack_miss(self, mock_output, mock_boolean):
+    def test_attack_attacker_miss(self, mock_output, mock_boolean):
         test_attacker = {"min_damage": 1,
                          "max_damage": 15,
                          "hit_rate": 1,
@@ -45,4 +45,27 @@ class TestAttack(TestCase):
         the_game_printed_this = mock_output.getvalue()
         expected_output = "attacker used \033[31mpunch\033[0m!\n" \
                           "attacker missed! | receiver HP:15\n\n"
+        self.assertEqual(expected_output, the_game_printed_this)
+
+    @patch('random.randint', return_value=20)
+    @patch('game.hit_or_miss', return_value=True)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_attack_attacker_dead(self, mock_output, mock_randint, mock_boolean):
+        test_attacker = {"min_damage": 5,
+                         "max_damage": 20,
+                         "hit_rate": 1,
+                         "HP": 15,
+                         "name": "attacker",
+                         "attack_move": "punch"}
+        test_receiver = {"min_damage": 1,
+                         "max_damage": 15,
+                         "hit_rate": 80,
+                         "HP": 20,
+                         "name": "receiver",
+                         "attack_move": "kick"}
+        attack(test_attacker, test_receiver)
+        the_game_printed_this = mock_output.getvalue()
+        expected_output = "attacker used \033[31mpunch\033[0m!\n" \
+                          "attacker dealt 20 damage! | receiver HP:0\n" \
+                          "receiver has died.\n\n"
         self.assertEqual(expected_output, the_game_printed_this)
